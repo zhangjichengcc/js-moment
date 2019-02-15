5cf3b8b922603b3a3a3b5883d41350f72452180e
zhangjc <zhangjc@hua-cloud.com.cn>
2019-02-15 09:44:15 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,70 @@
+/**
+ * 日期处理方法
+ *
+ * @date 2018-11-06
+ * @author zhangjicheng
+ */
+
+const moment = (date) => {
+  const now = new Date();
+  const time = date ? new Date(date) : now;
+  const formatRule = {
+    // 年
+    YYYY: time.getFullYear().toString(),
+    yyyy: time.getFullYear().toString(),
+    YY: time.getFullYear().toString().replace(/^[0-9]{2}([0-9]{2})$/,'$1'),
+    yy: time.getFullYear().toString().replace(/^[0-9]{2}([0-9]{2})$/,'$1'),
+    y: time.getFullYear().toString(),
+    Y: time.getFullYear().toString(),
+    // 月
+    MM: (time.getMonth()+1).toString().replace(/^([0-9]{1})$/,'0$1'),
+    M: (time.getMonth()+1).toString(),
+    // 日
+    DD: time.getDate().toString().replace(/^([0-9]{1})$/,'0$1'),
+    dd: time.getDate().toString().replace(/^([0-9]{1})$/,'0$1'),
+    D: time.getDate().toString(),
+    d: time.getDate().toString(),
+    // 时
+    HH: time.getHours().toString().replace(/^([0-9]{1})$/,'0$1'),
+    hh: time.getHours().toString().replace(/^([0-9]{1})$/,'0$1'),
+    H: time.getHours() > 12 ? (time.getHours() % 12).toString() : (time.getHours()).toString(),
+    h: time.getHours() > 12 ? (time.getHours() % 12).toString() : (time.getHours()).toString(),
+    // 分
+    mm: time.getMinutes().toString().replace(/^([0-9]{1})$/,'0$1'),
+    m: time.getMinutes().toString(),
+    // 秒
+    ss: time.getSeconds().toString().replace(/^([0-9]{1})$/,'0$1'),
+    s: time.getSeconds().toString(),
+  };
+  const defFormat = 'YYYY-MM-DD hh:mm:ss';
+  class Moment{
+    // 日期格式化
+    format(format = defFormat) {
+      let formatStr = format;
+      const keys = Object.keys(formatRule);
+      keys.forEach(key => {
+        formatStr = formatStr.replace(key, formatRule[key]);
+      })
+      this.formatDate = formatStr;
+      return formatStr;
+    }
+
+    // 日期算法
+    addDay(day) {
+      if(!time) return false;
+      const newDate = new Date(time.getTime()+day*24*60*60*1e3);
+      this.dataRes = newDate;
+      return newDate;
+    }
+
+    // getTime
+    getTime() {
+      const tiems = time.getTime();
+      this.tiems = tiems;
+      return tiems;
+    }
+  }
+  return new Moment(time);
+}
+
+export default moments;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  //插件依赖
+  plugins: [
+    // new htmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: 'src/index.html',
+    //   inject: 'body'
+    // }),
+    //清除前一次打包指定的文件夹
+    // new cleanWebpackPlugin(['dist'])
+  ],
+  //处理模块资源
+  module: {
+    rules: [
+      { //处理js文件
+        test: /\.js$/,
+        use: [{
+          loader: "babel-loader",
+          // options: {
+          //   //使用env预设来处理es6语法的js文件
+          //   presets: ['env']
+          // }
+        }],
+        exclude: [
+          path.resolve(__dirname, './node_modules')
+        ]
+      },
+    ]
+  }
+};

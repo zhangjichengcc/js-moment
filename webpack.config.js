78340b00d54123e0c6640698358c8e7dc64ba2aa
zhangjichengcc <zhangjichengcc@163.com>
2021-04-02 00:22:13 +0800

    change ts

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   mode: 'production',
   module: {
     rules: [

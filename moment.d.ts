30dd2f7b32715fd5fdb90573a36e13dbb3f9164d
zhangjicheng <zhangjichengcc@163.com>
2022-05-25 19:01:16 +0800

    update

--- moment.d.ts
+++ moment.d.ts
@@ -2,17 +2,11 @@
  * @Author: zhangjicheng
  * @Date: 2022-05-11 15:08:55
  * @LastEditors: zhangjicheng
- * @LastEditTime: 2022-05-25 09:55:17
+ * @LastEditTime: 2022-05-25 17:39:27
  * @FilePath: \js-moment\moment.d.ts
  * @js-moment ts类型声明文件
  */
 
-// import { Moment } from './src/unit';
 import moment from './src/index';
-
-/**
- * js-moment 日期格式化组件
- */
-// declare function moment(date?: string | Date) : Moment;
 
 export default moment;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangjicheng
  * @Date: 2019-02-18 11:41:51
- * @LastEditTime: 2022-05-25 14:39:52
+ * @LastEditTime: 2022-05-25 16:35:48
  * @LastEditors: zhangjicheng
  * @Description: 
  * @FilePath: \js-moment\webpack.config.ts
@@ -34,11 +34,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
+    // https://webpack.js.org/configuration/output/#outputglobalobject
+    library: 'moment',
     globalObject: 'this',
-    library: {
-      name: 'moment',
-      type: 'umd', // 将你的 library 暴露为所有的模块定义下都可运行的方式
-    },
+    libraryTarget: 'umd', // 将你的 library 暴露为所有的模块定义下都可运行的方式
   },
   optimization: {
     minimize: !!isMinify,
